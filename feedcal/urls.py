bc545f12546fe587a3d6796f6ff666f3c1ca917b
Paul Traylor <kungfudiscomonkey@gmail.com>
2015-12-22 11:13:17 +0900

    Update urls for Django 1.9

--- feedcal/urls.py
+++ feedcal/urls.py
@@ -1,12 +1,11 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
 
 import feedcal.views
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'^$', login_required(feedcal.views.IndexView.as_view()), name='index'),
     url(r'^(?P<uuid>[^/]+)/pie/$', login_required(feedcal.views.PieView.as_view()), name='pie'),
     url(r'^(?P<uuid>[^/]+)/bar/$', login_required(feedcal.views.BarView.as_view()), name='bar'),
     url(r'^parse/$', feedcal.views.ParseView.as_view(), name='parse'),
-)
+]
